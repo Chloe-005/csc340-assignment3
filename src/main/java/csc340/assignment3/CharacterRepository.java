530117465081a8ed1264f6b4caec327d9ea65911
Chloe Rhodes <carhodes@uncg.edu>
2026-02-27 16:19:00 -0500

    Added a custom query

--- src/main/java/csc340/assignment3/CharacterRepository.java
+++ src/main/java/csc340/assignment3/CharacterRepository.java
@@ -1,5 +1,17 @@
 package csc340.assignment3;
 
-public class CharacterRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Long> {
+
+  List<Character> findByUniverse(String universe);
+
+  @Query(value = "SELECT s.* FROM characters s WHERE s.name like %?1%", nativeQuery = true)
+  List<Character> findByName(String name);
+
 }
