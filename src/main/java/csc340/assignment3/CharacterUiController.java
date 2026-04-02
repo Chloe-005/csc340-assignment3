3385a6c08e48463954ada2d4042aa92bd85d051d
Chloe Rhodes <carhodes@uncg.edu>
2026-04-02 13:19:47 -0400

    In class setup help

---
+++ src/main/java/csc340/assignment3/CharacterUiController.java
@@ -0,0 +1,37 @@
+package csc340.assignment3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/characters")
+public class CharacterUiController {
+    
+    @Autowired
+    private CharacterService characterService;
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("/{id}")
+    public String getCharacterById(@PathVariable Long id, Model model) {
+        Character character = characterService.getCharacterById(id);
+        model.addAttribute("character", character);
+        if (character != null) {
+            return "character-details";
+        } else {
+            return "about";
+        }
+    }
+
+    @GetMapping("/add")
+    public String showForm(){
+        return "new-character-form";
+    }
+}
